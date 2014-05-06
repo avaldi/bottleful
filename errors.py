e7389e5ee0a17c3ad7a24def5dcda8d07b1baabc
AKK <kazi@dubizzle.com>
2014-05-06 14:33:13 +0400

    Minor changes and PEP-8

--- errors.py
+++ errors.py
@@ -1,15 +1,17 @@
-import traceback
 import sys
+import traceback
 
 from bottle import HTTPError
 
 
 class APIError(HTTPError):
-    """ Main error exception. It subclass bottle.HTTPError automatically
-        adding the traceback to the response.
+    """ Main error exception. It subclass bottle.HTTPError automatically and
+    adds the traceback to the response.
     """
 
     def __init__(self, *args, **kwargs):
+        """ Attaches the traceback if it exists.
+        """
 
         if sys.exc_info()[0]:
             kwargs['traceback'] = traceback.format_exc()
