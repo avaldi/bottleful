9781f38b22fa19d64cad0f93c72f19380f31a7e8
Andrea Valdi <andrea@dubizzle.com>
2014-05-19 11:49:44 +0400

    [trailing_slash_mw] Changed variable names to more significant

--- middlewares.py
+++ middlewares.py
@@ -1,14 +1,16 @@
 
 
 class StripPathMiddleware(object):
-    """ Middleware to strip out slashes from URLs
+    """ Middleware to strip out slashes from URLs.
+    Shamelessly stolen from:
+      http://bottlepy.org/docs/dev/recipes.html#ignore-trailing-slashes
     """
 
     def __init__(self, app):
         self.app = app
 
-    def __call__(self, e, h):
+    def __call__(self, environ, start_response):
         """ Remove all the trailing slashes from the requested URL
         """
-        e['PATH_INFO'] = e['PATH_INFO'].rstrip('/')
-        return self.app(e,h)
+        environ['PATH_INFO'] = environ['PATH_INFO'].rstrip('/')
+        return self.app(environ, start_response)
