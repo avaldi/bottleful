2fc3b4b16631b3114452cc02d19730ad0ce972ba
Andrea Valdi <andrea@dubizzle.com>
2014-05-20 17:02:33 +0400

    [user_schemas] APIError refactoring

--- errors.py
+++ errors.py
@@ -9,11 +9,18 @@ class APIError(HTTPError):
     adds the traceback to the response.
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, status=None, body=None, exception=None, traceback=None,
+                 **options):
         """ Attaches the traceback if it exists.
         """
 
         if sys.exc_info()[0]:
-            kwargs['traceback'] = traceback.format_exc()
+            traceback = traceback.format_exc()
 
-        super(APIError, self).__init__(*args, **kwargs)
+        super(APIError, self).__init__(
+            status=status,
+            body=body,
+            exception=exception,
+            traceback=traceback,
+            **options
+        )
